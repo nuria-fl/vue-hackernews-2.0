2f91de186a1048466cf7c6dc312bbfe8d5cf2209
Nuria Soriano <nuria@blended.io>
2017-01-17 17:30:36 +0100

    simple ssr

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   preserveWhitespace: false,
+  buble: {
+    // enable object spread operator
+    // NOTE: you need to provide Object.assign polyfill yourself!
+    objectAssign: 'Object.assign',
+
+  },
   postcss: [
     require('autoprefixer')({
       browsers: ['last 3 versions']
---
+++ src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button type="button" v-on:click="toggle">Click event</button>
+    <transition name="fade">
+      <div v-if="showThis">
+        Now you see me
+      </div>
+    </transition>
+    <hr>
+    <button type="button" v-on:click="increment">Increment</button>
+    <p>State: {{count}}</p>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+        msg: 'Hello world!',
+        showThis: false
+    }
+  },
+  computed: {
+    count () {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    toggle () {
+      return this.showThis = !this.showThis
+    },
+    ...mapMutations(['increment'])
+  }
+}
+</script>
